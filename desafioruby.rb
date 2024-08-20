ce98339f73e32d6344253bf55c7dc90f1f7d5edf
Leydianne da Silva Ferreira <luvlivros@gmail.com>
2024-08-20 15:48:50 -0300

    desafioruby.rb

---
+++ desafioruby.rb
@@ -0,0 +1,11 @@
+puts "Informe o 1° número";
+firstValue = gets.to_i;
+
+puts "Informe o 2° número"
+secondValue = gets.to_i;
+
+#TODO:  Crie as condições necessárias para calcular PROD
+
+PROD = firstValue * secondValue;
+
+puts "PROD = #{PROD}";
